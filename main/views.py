76cb4113ca8b5705dacc2f4c0e4b0309a023389a
jyoshin <gleb.parkdes@gmail.com>
2023-12-05 18:49:08 +0600

    Done

--- main/views.py
+++ main/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+import requests
+import datetime
 
 def home(request):
-    return HttpResponse('Hello, world')
+    if 'city' in request.POST:
+        city = request.POST['city']
+    else:
+        city = 'Almaty'
+
+    appid = 'a440dfb0ec7bcc037444b8dfef4afa4c'
+    URL = 'https://api.openweathermap.org/data/2.5/weather'
+    PARAMS = {'q':city,'appid':appid, 'units':'metric'}
+
+    r = requests.get(url=URL, params=PARAMS)
+
+    res = r.json()
+    description = res['weather'][0]['description']
+    icon = res['weather'][0]['icon']
+    temp = res['main']['temp']
+
+    day = datetime.date.today()
+
+    return render(request, 'base/base.html', {'description':description, 'icon':icon, 'temp':temp, 'day':day, 'city':city})
+
